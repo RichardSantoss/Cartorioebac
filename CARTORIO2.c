1cfdbc169e036759efdc97bfab741d746d9c5ded
RichardSantoss <107050237+RichardSantoss@users.noreply.github.com>
2024-04-12 00:34:19 -0300

    Sistema de login e senha
    Adicionei um sistema onde caso alguém erre o login ou senha, o aplicativo não dará continuidade.

--- CARTORIO2.c
+++ CARTORIO2.c
@@ -112,54 +112,90 @@ int deletar()
 
 int main()
 { 
+    setlocale(LC_ALL, "Portuguese");
     int opcao=0; // Definindo variáveis
     int x=1;  
-      
-    for(x=1;x=1;)
-    {
-	
-	  system("cls");
-	 
-     setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+    char senhadigitada[10]="a";
+    char logindigitado[10]="a";   
+    int comparacao;
     
-	  printf("### Cartório da EBAC ###\n\n"); //ínicio do menu	
-	  printf("Escolha a opção desejada do menu: \n\n");
-      printf("\t1 - Registrar nomes\n");
-      printf("\t2 - Consultar nomes\n");
-      printf("\t3 - Deletar nomes\n\n");
-      printf("\t4 - Sair do sistema\n\n");
-	  printf("Opção:" ); //Fim do menu
+    
+    printf("### Cartório da EBAC ###\n\n");
+    printf("Login de administrador! \n\nDigite o seu login: ");
+    scanf("%s",logindigitado);
+    
+    comparacao = strcmp(logindigitado,"richardsantos");
+	  
+    printf("\nDigite a sua senha: ");
+    scanf("%s",senhadigitada);
+    
+    comparacao = strcmp(senhadigitada,"admin");
+    
+    
+				   
+    if(comparacao == 0)
+    {
+    	
+    	   	 		        
+   		 for(x=1;x=1;)
+   		 {
+	
+		  system("cls");
+	 
+    	 setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+    
+	 	 printf("### Cartório da EBAC ###\n\n"); //ínicio do menu	
+	 	 printf("Escolha a opção desejada do menu: \n\n");
+     	 printf("\t1 - Registrar nomes\n");
+     	 printf("\t2 - Consultar nomes\n");
+     	 printf("\t3 - Deletar nomes\n\n");
+     	 printf("\t4 - Sair do sistema\n\n");
+	   	 printf("Opção:" ); //Fim do menu
          
-      scanf("%d", &opcao);//Armazenando a escolha do usuário
+      	scanf("%d", &opcao);//Armazenando a escolha do usuário
          
-      system("cls");//comando usado para "limpar" tela de menu
+     	 system("cls");//comando usado para "limpar" tela de menu
       
-      switch(opcao)// inicio da seleção do menu
-      {
-      	case 1:
-      	registro();//chamada de funções
-		break;
+     	 switch(opcao)// inicio da seleção do menu
+     	 	{
+      		case 1:
+      		registro();//chamada de funções
+			break;
 		
-		case 2:
-		consulta();
-		break;
+			case 2:
+			consulta();
+			break;
 		
-		case 3:	
-        deletar();   
-	 	break;
+			case 3:	
+        	deletar();   
+	 		break;
 	 	
-	 	case 4:
-	 	printf("Obrigado por utilizar o sistema!");
-	 	return 0;
-	 	break;
+	 		case 4:
+	 		printf("Obrigado por utilizar o sistema!");
+	 		return 0;
+	 		break;
 	 	
-	 	default:
-	 	printf("Essa opção não está disponivel! Tente novamente\n");
-	 	system("pause");
-	 	break;
-	  }
-    }
-}
+	 		default:
+	 		printf("Essa opção não está disponivel! Tente novamente\n");
+	 		system("pause");
+	 		break;
+	     }	
+	 		
+	 		
+	 		
+	 		
+	 		
+	 		
+	  		}	  		
+   	   
+		  
+		    	      	     	   
+   	}else
+		printf("Senha incorreta!");
+		
+ }
+	    
+
 
 
 
